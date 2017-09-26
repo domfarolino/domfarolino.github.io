e5a5f86d7e49fa82a9379cdc8b15ea08cf4336fa
domfarolino <domfarolino@gmail.com>
2017-09-26 14:10:43 -0400

    Only print rendering errors if they exist

--- render.js
+++ render.js
@@ -2,7 +2,7 @@ const path = require('path'), fs = require('fs'), ejs = require("ejs");
 
 function renderFile(fileName) {
   ejs.renderFile(path.join(fileName), {partial: false}, function(err, document) {
-    console.log(err);
+    if (err) console.log(err);
     const outputFileName = `${fileName.split('.')[0]}.html`;
     fs.writeFileSync(path.join(outputFileName), document, 'utf8');
   });
