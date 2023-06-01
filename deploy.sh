3166c5dd8fb915019e0c6536e4f2f53be53217a5
Dominic Farolino <domfarolino@gmail.com>
2023-06-01 08:06:00 -0400

    Fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,4 @@
 git branch -D master;
-cd ./lib/client && ../../node_modules/webpack/bin/webpack.js && cd ../..
 git checkout -b master &&
 rm -rf ./public/js/.gitignore
 cp -r ./public/* ./ && rm -R ./public/* &&
