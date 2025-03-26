64d7d8d6d8dbb7b3e68121ba816473f31b5359f2
Dominic Farolino <domfarolino@gmail.com>
2025-03-26 00:06:15 -0400

    Add more quotes

--- public/quotes/quotes.js
+++ public/quotes/quotes.js
@@ -4,12 +4,6 @@
 //   - Don't have to worry about trailing commas
 module.exports = [
   {
-    "id": "in-italy",
-    "quote": "In Italy for thirty years under the Borgias, they had warfare, terror, murder, and bloodshed, but they produced Michelangelo, Leonardo da Vinci, and the Renaissance. In Switzerland, they had brotherly love, they had five hundred years of democracy and peace, and what did that produce? The cuckoo clock.",
-    "author": "Harry Lime, <em>The Third Man</em>",
-    "source": "https://www.laphamsquarterly.org/politics/miscellany/history-lesson-orson-welles#:~:text=In%20The%20Third%20Man%2C%20Orson,da%20Vinci%2C%20and%20the%20Renaissance."
-  },
-  {
     "id": "too-much-agreement",
     "quote": "Too much agreement kills a chat.",
     "author": "Eldridge Cleaver",
@@ -65,6 +59,12 @@ module.exports = [
     "author": "Voltaire",
     "source": "https://www.goodreads.com/quotes/20527-those-who-can-make-you-believe-absurdities-can-make-you"
   },
+  {
+    "id": "wicked",
+    "quote": "It is crazy to want what is impossible. And impossible for the wicked not to do so.",
+    "author": "Marcus Aurelius",
+    "source": "https://www.goodreads.com/quotes/8054438-it-is-crazy-to-want-what-is-impossible-and-impossible"
+  },
   // Experts.
   {
     "id": "science-is-the-belief",
@@ -84,12 +84,29 @@ module.exports = [
     "author": "Paul Pusateri",
     "source": "https://milliganpusateri.com/staff/paul-pusateri/"
   },
-  // TODO: Sowell quote about when the educated lie.
   {
     "id": "better-to-grasp-reality",
     "quote": "It is far better to grasp the Universe as it really is than to persist in delusion, however satisfying and reassuring.",
     "author": "Carl Sagan",
     "source": "https://www.goodreads.com/quotes/3882-for-me-it-is-far-better-to-grasp-the-universe"
+  },
+  {
+    "id": "educated-ignorance",
+    "quote": "Of all ignorance, the ignorance of the educated is the most dangerous. Not only are educated people likely to have more influence, they are the last people to suspect that they don't know what they are talking about when they go outside their narrow fields.",
+    "author": "Thomas Sowell",
+    "source": "https://x.com/ThomasSowell/status/1379126480177831936"
+  },
+  {
+    "id": "decisions-price-for-being-wrong",
+    "quote": "It is hard to imagine a more stupid or more dangerous way of making decisions than by putting those decisions in the hands of people who pay no price for being wrong.",
+    "author": "Thomas Sowell",
+    "source": "https://www.goodreads.com/quotes/3823206-it-is-hard-to-imagine-a-more-stupid-or-more"
+  },
+  {
+    "id": "there-are-no-solutions",
+    "quote": "There are no solutions. There are only trade-offs.",
+    "author": "Thomas Sowell",
+    "source": "https://www.goodreads.com/quotes/1411380-there-are-no-solutions-there-are-only-trade-offs"
   },
   {
     "id": "false-reality-of-inferior-status",
@@ -112,7 +129,7 @@ module.exports = [
   },
   {
     "id": "he-winces-not",
-    "quote": "I sit with Shakespeare, and he winces not. Across the color line I move arm and arm with Balzac and Dumas, where smiling men and welcoming women glide in gilded halls. From out of the caves of evening that swing between the strong-limbed Earth and the tracery of stars, I summon Aristotle and Aurelius and what soul I will, and they come all graciously with no scorn nor condescension. So, wed with Truth, I dwell above the veil. Is this the life you grudge us, O knightly America? Is this the life you long to change into the dull red hideousness of Georgia? Are you so afraid lest peering from this high Pisgah, between Philistine and Amalekite, we sight the Promised Land?",
+    "quote": "&nbsp;&nbsp;&nbsp;I sit with Shakespeare, and he winces not. Across the color line I move arm and arm with Balzac and Dumas, where smiling men and welcoming women glide in gilded halls. From out of the caves of evening that swing between the strong-limbed Earth and the tracery of stars, I summon Aristotle and Aurelius and what soul I will, and they come all graciously with no scorn nor condescension.<br><br>&nbsp;&nbsp;&nbsp;So, wed with Truth, I dwell above the veil. Is this the life you grudge us, O knightly America? Is this the life you long to change into the dull red hideousness of Georgia? Are you so afraid lest peering from this high Pisgah, between Philistine and Amalekite, we sight the Promised Land?",
     "author": "W.E.B. Dubois",
     "source": "https://www.goodreads.com/quotes/760755-i-sit-with-shakespeare-and-he-winces-not-across-the"
   },
@@ -130,6 +147,12 @@ module.exports = [
     "author": "Chris Williamson",
     "source": "https://youtu.be/WEP5ubPMGDU?si=Rp48ISYbGHISsXmo&t=9639"
   },
+  {
+    "id": "in-italy",
+    "quote": "In Italy for thirty years under the Borgias, they had warfare, terror, murder, and bloodshed, but they produced Michelangelo, Leonardo da Vinci, and the Renaissance. In Switzerland, they had brotherly love, they had five hundred years of democracy and peace, and what did that produce? The cuckoo clock.",
+    "author": "Harry Lime, <em>The Third Man</em>",
+    "source": "https://www.laphamsquarterly.org/politics/miscellany/history-lesson-orson-welles#:~:text=In%20The%20Third%20Man%2C%20Orson,da%20Vinci%2C%20and%20the%20Renaissance."
+  },
   // Movement/revolutions betraying the ideals that started it.
   {
     "id": "great-cause",
@@ -138,16 +161,16 @@ module.exports = [
     "source": "https://www.goodreads.com/quotes/98215-every-great-cause-begins-as-a-movement-becomes-a-business"
   },
   {
-    "id": "insurgencies",
-    "quote": "&nbsp;&nbsp;&nbsp;One of these patterns in the history of many insurgent movements has been a disappointment in the direction that the movement has taken after victory, including claims that the revolution has been \"betrayed\" and that the later leaders and followers have failed to live up to the high standards set by their predecessors in the insurgency. None of this is surprising when such movements are examined in terms of their processes, including the incentives and constraints at work. <br><br>&nbsp;&nbsp;&nbsp;First of all, the kinds of people attracted to the original insurgency, under the initial set of incentives and constraints, tend to be very different from the kinds of people who gravitate to it after it has become successful and achieved a major part of its goals. By definition, an insurgent movement forms under a set of incentives and constraints very different from those which it seeks to create. Often the members face a certain amount of hostility, or even persecution, from those around them or from an elite currently benefitting from the status quo. These original insurgents may even face dangers to their careers or to their lives. These are not conditions which tend to attract timid careerists or mere opportunists, unless the opportunists foresee a high probability that the insurgency will succeed within period of time that is relevant to their personal ambitions. <br><br>&nbsp;&nbsp;&nbsp;After the success of the insurgency, however, radically different incentives and constraints are created by that very success. Many Christians in the Roman Empire, for example, went from being a poor and persecuted minority to being among the powerful agents of a state religion, able to enrich themselves and to persecute others. A similar pattern marked the history of the Communists in Russia many centuries later. On a smaller scale, the life cycle of regulatory agencies in the United States has often been seen to follow a pattern leading to control by very different kinds of people and policies from those behind the movement which led to the creation of the agency in the first place.",
-    "author": "Thomas Sowell, Knowledge and Decisions",
-    "source": "https://www.google.com/books/edition/Knowledge_And_Decisions/-a1UEAAAQBAJ?hl=en&gbpv=1&dq=revolution+leaders+goal+inauthor:Thomas+inauthor:Sowell&pg=PT10&printsec=frontcover",
-  },
-  {
     "id": "destroying-the-engines",
     "quote": "However [political parties] may now and then answer popular ends, they are likely in the course of time and things, to become potent engines, by which cunning, ambitious, and unprincipled men will be enabled to subvert the power of the people and to usurp for themselves the reins of government, destroying afterwards the very engines which have lifted them to unjust dominion.",
     "author": "George Washington",
     "source": "https://www.goodreads.com/quotes/462873-however-political-parties-may-now-and-then-answer-popular-ends"
+  },
+  {
+    "id": "insurgencies",
+    "quote": "&nbsp;&nbsp;&nbsp;One of these patterns in the history of many insurgent movements has been a disappointment in the direction that the movement has taken after victory, including claims that the revolution has been \"betrayed\" and that the later leaders and followers have failed to live up to the high standards set by their predecessors in the insurgency. None of this is surprising when such movements are examined in terms of their processes, including the incentives and constraints at work. <br><br>&nbsp;&nbsp;&nbsp;First of all, the kinds of people attracted to the original insurgency, under the initial set of incentives and constraints, tend to be very different from the kinds of people who gravitate to it after it has become successful and achieved a major part of its goals. By definition, an insurgent movement forms under a set of incentives and constraints very different from those which it seeks to create. Often the members face a certain amount of hostility, or even persecution, from those around them or from an elite currently benefitting from the status quo. These original insurgents may even face dangers to their careers or to their lives. These are not conditions which tend to attract timid careerists or mere opportunists, unless the opportunists foresee a high probability that the insurgency will succeed within period of time that is relevant to their personal ambitions. <br><br>&nbsp;&nbsp;&nbsp;After the success of the insurgency, however, radically different incentives and constraints are created by that very success. Many Christians in the Roman Empire, for example, went from being a poor and persecuted minority to being among the powerful agents of a state religion, able to enrich themselves and to persecute others. A similar pattern marked the history of the Communists in Russia many centuries later. On a smaller scale, the life cycle of regulatory agencies in the United States has often been seen to follow a pattern leading to control by very different kinds of people and policies from those behind the movement which led to the creation of the agency in the first place.",
+    "author": "Thomas Sowell, Knowledge and Decisions",
+    "source": "https://www.google.com/books/edition/Knowledge_And_Decisions/-a1UEAAAQBAJ?hl=en&gbpv=1&dq=revolution+leaders+goal+inauthor:Thomas+inauthor:Sowell&pg=PT10&printsec=frontcover",
   },
   {
     "id": "champagne",
@@ -162,9 +185,48 @@ module.exports = [
     "source": "https://www.goodreads.com/quotes/19682-all-of-humanity-s-problems-stem-from-man-s-inability-to-sit"
   },
   {
+    "id": "one-is-a-statistic",
+    "quote": "One death is a tragedy; a million is a statistic.",
+    "author": "Joseph Stalin",
+    "source": "https://www.goodreads.com/quotes/140634-those-who-would-give-up-essential-liberty-to-purchase-a",
+    "related": ["https://www.goodreads.com/quotes/7253432-one-death-is-a-tragedy-a-million-is-a-statistic"]
+  },
+  {
+    "id": "liberty-safety",
+    "quote": "Those who would give up essential liberty to purchase a little temporary safety, deserve neither liberty nor safety.",
+    "author": "Benjamin Franklin",
+    "source": "https://www.goodreads.com/quotes/140634-those-who-would-give-up-essential-liberty-to-purchase-a"
+  },
+  {
+    "id": "price-of-freedom",
+    "quote": "The Price of Freedom Is Eternal Vigilance.",
+    "author": "John Philpot Curran; Thomas Jefferson",
+    "source": "https://www.goodreads.com/quotes/401-the-price-of-freedom-is-eternal-vigilance",
+    "related": ["https://www.monticello.org/research-education/thomas-jefferson-encyclopedia/eternal-vigilance-price-liberty-spurious-quotation/"]
+  },
+  {
+    "id": "climate-doomer",
+    "quote": "There's a certain species of doomer environmentalist who talks about their dread of what climate change will do to us in such a way that you cannot possibly miss the intensity of their longing for that exact outcome.",
+    "author": "Freddie deBoar",
+    "source": "https://freddiedeboer.substack.com/p/ai-or-the-eternal-recurrence-of-hubris"
+  },
+
+  {
+    "id": "studied-grace",
+    "quote": "Studied grace is not grace.",
+    "author": "Ottessa Moshfegh",
+    "source": "https://www.goodreads.com/book/show/44279110-my-year-of-rest-and-relaxation"
+  },
+  {
     "id": "god-shaped-hole",
     "quote": "There is a God-shaped vacuum in the heart of each man which cannot be satisfied by any created thing but only by God the Creator.",
     "author": "Blaise Pascal",
     "source": "https://www.goodreads.com/quotes/801132-there-is-a-god-shaped-vacuum-in-the-heart-of-each"
+  },
+  {
+    "id": "high-performance",
+    "quote": "Somehow, hours of poor performance are required for moments of high performance.",
+    "author": "Dominic Farolino",
+    "source": "https://domfarolino.com"
   }
 ];
