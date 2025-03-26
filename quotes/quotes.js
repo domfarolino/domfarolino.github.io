fe4a8b61cab34b2c2bc2ed55cce3a686e93db3a2
Dominic Farolino <domfarolino@gmail.com>
2025-03-26 00:15:56 -0400

    This commit will be automatically published

---
+++ quotes/quotes.js
@@ -0,0 +1,231 @@
+// The "database" of quotes is stored as a JavaScript module, instead of
+// standard JSON, so that I:
+//   - Can use comments to annotate certain things.
+//   - Don't have to worry about trailing commas
+module.exports = [
+  {
+    "id": "too-much-agreement",
+    "quote": "Too much agreement kills a chat.",
+    "author": "Eldridge Cleaver",
+    "source": "https://www.goodreads.com/quotes/7776608-too-much-agreement-kills-chat"
+  },
+  {
+    "id": "dangerous-to-be-right",
+    "quote": "It is dangerous to be right in matters on which the established authorities are wrong.",
+    "author": "Voltaire",
+    "source": "https://www.goodreads.com/quotes/28326-it-is-dangerous-to-be-right-in-matters-on-which"
+  },
+  {
+    "id": "he-who-dares",
+    "quote": "He who dares not offend cannot be honest.",
+    "author": "Thomas Paine",
+    "source": "https://www.goodreads.com/quotes/889423-he-who-dares-not-offend-cannot-be-honest"
+  },
+  {
+    "id": "he-who-knows",
+    "quote": "He who knows only his own side of the case knows little of that. His reasons may be good, and no one may have been able to refute them. But if he is equally unable to refute the reasons on the opposite side, if he does not so much as know what they are, he has no ground for preferring either opinion.",
+    "author": "John Stuart Mill",
+    "source": "https://www.goodreads.com/quotes/66643-he-who-knows-only-his-own-side-of-the-case"
+  },
+  {
+    "id": "nietzsche-change-our-mind",
+    "quote": "When we have to change our mind about a person, we hold the inconvenience he causes us very much against him.",
+    "author": "Friedrich Nietzsche",
+    "source": "https://www.goodreads.com/quotes/746253-when-we-have-to-change-our-mind-about-a-person",
+    "related": ["https://www.goodreads.com/quotes/408906-when-we-have-to-change-an-opinion-about-any-one"]
+  },
+  {
+    "id": "salary-depends-on-not-understanding",
+    "quote": "It is difficult to get a man to understand something, when his salary depends on his not understanding it.",
+    "author": "Upton Sinclair",
+    "source": "https://www.goodreads.com/quotes/21810-it-is-difficult-to-get-a-man-to-understand-something"
+  },
+  {
+    "id": "stand-for-the-truth",
+    "quote": "To stand up for the truth is nothing! For truth you have to sit in jail!",
+    "author": "Aleksandr Solzhenitsyn",
+    "source": "https://www.goodreads.com/quotes/11349440-to-stand-up-for-the-truth-is-nothing-for-truth",
+    "related": ["https://www.goodreads.com/quotes/188663-to-stand-up-for-truth-is-nothing-for-truth-you"]
+  },
+  {
+    "id": "living-in-reaction",
+    "quote": "Rebellion is as much of a cage as obedience is. They both mean living in reaction to someone else’s way instead of forging your own.",
+    "author": "Glennon Doyle",
+    "source": "https://www.goodreads.com/quotes/10286319-rebellion-is-as-much-of-a-cage-as-obedience-is"
+  },
+  {
+    "id": "absurdities-atrocities",
+    "quote": "Those who can make you believe absurdities, can make you commit atrocities.",
+    "author": "Voltaire",
+    "source": "https://www.goodreads.com/quotes/20527-those-who-can-make-you-believe-absurdities-can-make-you"
+  },
+  {
+    "id": "wicked",
+    "quote": "It is crazy to want what is impossible. And impossible for the wicked not to do so.",
+    "author": "Marcus Aurelius",
+    "source": "https://www.goodreads.com/quotes/8054438-it-is-crazy-to-want-what-is-impossible-and-impossible"
+  },
+  {
+    "id": "climate-doomer",
+    "quote": "There's a certain species of doomer environmentalist who talks about their dread of what climate change will do to us in such a way that you cannot possibly miss the intensity of their longing for that exact outcome.",
+    "author": "Freddie deBoar",
+    "source": "https://freddiedeboer.substack.com/p/ai-or-the-eternal-recurrence-of-hubris"
+  },
+  // Experts.
+  {
+    "id": "science-is-the-belief",
+    "quote": "Science is the belief in the ignorance of experts.",
+    "author": "Richard Feynman",
+    "source": "https://www.goodreads.com/quotes/3225333-science-is-the-belief-in-the-ignorance-of-experts"
+  },
+  {
+    "id": "sacred-answers",
+    "quote": "I would rather have questions that can't be answered than answers that can’t be questioned.",
+    "author": "Richard Feynman",
+    "source": "https://www.goodreads.com/quotes/1134331-i-would-rather-have-questions-that-can-t-be-answered-than"
+  },
+  {
+    "id": "hidden-motivation",
+    "quote": "Science with a hidden motivation is worse than a lie. Because it’s more difficult to identify.",
+    "author": "Paul Pusateri",
+    "source": "https://milliganpusateri.com/staff/paul-pusateri/"
+  },
+  {
+    "id": "better-to-grasp-reality",
+    "quote": "It is far better to grasp the Universe as it really is than to persist in delusion, however satisfying and reassuring.",
+    "author": "Carl Sagan",
+    "source": "https://www.goodreads.com/quotes/3882-for-me-it-is-far-better-to-grasp-the-universe"
+  },
+  {
+    "id": "educated-ignorance",
+    "quote": "Of all ignorance, the ignorance of the educated is the most dangerous. Not only are educated people likely to have more influence, they are the last people to suspect that they don't know what they are talking about when they go outside their narrow fields.",
+    "author": "Thomas Sowell",
+    "source": "https://x.com/ThomasSowell/status/1379126480177831936"
+  },
+  {
+    "id": "decisions-price-for-being-wrong",
+    "quote": "It is hard to imagine a more stupid or more dangerous way of making decisions than by putting those decisions in the hands of people who pay no price for being wrong.",
+    "author": "Thomas Sowell",
+    "source": "https://www.goodreads.com/quotes/3823206-it-is-hard-to-imagine-a-more-stupid-or-more"
+  },
+  {
+    "id": "there-are-no-solutions",
+    "quote": "There are no solutions. There are only trade-offs.",
+    "author": "Thomas Sowell",
+    "source": "https://www.goodreads.com/quotes/1411380-there-are-no-solutions-there-are-only-trade-offs"
+  },
+  {
+    "id": "false-reality-of-inferior-status",
+    "quote": "It is the peculiar triumph of society—and its loss—that it is able to convince those people to whom it has given inferior status of the reality of this decree.",
+    "author": "James Baldwin",
+    "source": "https://www.goodreads.com/quotes/11731340-it-is-the-peculiar-triumph-of-society-and-its-loss-that-it"
+  },
+  // It will happen one way or another. Do you want to get out in front of it, or let your enemies?
+  {
+    "id": "chaos-order",
+    "quote": "If you ratchet up the chaos enough, people will crave order so much they’re not gonna give a shit who gives it to them.",
+    "author": "Konstantin Kisin",
+    "source": "https://youtu.be/x4Ha8yeXuU8?si=XGA7xldAjNitjzsr&t=6853"
+  },
+  {
+    "id": "borders",
+    "quote": "If liberals won’t police borders, then fascists will.",
+    "author": "David Frum",
+    "source": "https://x.com/davidfrum/status/1858658448977821934"
+  },
+  {
+    "id": "he-winces-not",
+    "quote": "&nbsp;&nbsp;&nbsp;I sit with Shakespeare, and he winces not. Across the color line I move arm and arm with Balzac and Dumas, where smiling men and welcoming women glide in gilded halls. From out of the caves of evening that swing between the strong-limbed Earth and the tracery of stars, I summon Aristotle and Aurelius and what soul I will, and they come all graciously with no scorn nor condescension.<br><br>&nbsp;&nbsp;&nbsp;So, wed with Truth, I dwell above the veil. Is this the life you grudge us, O knightly America? Is this the life you long to change into the dull red hideousness of Georgia? Are you so afraid lest peering from this high Pisgah, between Philistine and Amalekite, we sight the Promised Land?",
+    "author": "W.E.B. Dubois",
+    "source": "https://www.goodreads.com/quotes/760755-i-sit-with-shakespeare-and-he-winces-not-across-the"
+  },
+  // Life is too easy; too comfortable.
+  {
+    "id": "lack-itself",
+    "quote": "As we make the world easier and easier, at some point, what becomes lacking is lack itself. This is the problem with the material plenty that's at hand.",
+    "author": "Søren Kierkegaard; Jordan Peterson",
+    "source": "https://youtu.be/WEP5ubPMGDU?feature=shared&t=9486",
+    "related": ["https://www.goodreads.com/quotes/11044020-so-there-i-sat-and-smoked-my-cigar-until-i#:~:text=So%20only%20one%20lack%20remains%20%5Bin%20our%20time%5D%2C%20even%20though%20not%20yet%20felt%2C%20the%20lack%20of%20difficulty"]
+  },
+  {
+    "id": "heavy-things",
+    "quote": "Given that we're in a world that is comfortable, we have created buildings in which there are heavy things that you pick up and put down in the same place, because it's simply so rare that you have to pick up heavy things.",
+    "author": "Chris Williamson",
+    "source": "https://youtu.be/WEP5ubPMGDU?si=Rp48ISYbGHISsXmo&t=9639"
+  },
+  {
+    "id": "in-italy",
+    "quote": "In Italy for thirty years under the Borgias, they had warfare, terror, murder, and bloodshed, but they produced Michelangelo, Leonardo da Vinci, and the Renaissance. In Switzerland, they had brotherly love, they had five hundred years of democracy and peace, and what did that produce? The cuckoo clock.",
+    "author": "Harry Lime, <em>The Third Man</em>",
+    "source": "https://www.laphamsquarterly.org/politics/miscellany/history-lesson-orson-welles#:~:text=In%20The%20Third%20Man%2C%20Orson,da%20Vinci%2C%20and%20the%20Renaissance."
+  },
+  // Movement/revolutions betraying the ideals that started it.
+  {
+    "id": "great-cause",
+    "quote": "Every great cause begins as a movement, becomes a business, and eventually degenerates into a racket.",
+    "author": "Eric Hoffer",
+    "source": "https://www.goodreads.com/quotes/98215-every-great-cause-begins-as-a-movement-becomes-a-business"
+  },
+  {
+    "id": "destroying-the-engines",
+    "quote": "However [political parties] may now and then answer popular ends, they are likely in the course of time and things, to become potent engines, by which cunning, ambitious, and unprincipled men will be enabled to subvert the power of the people and to usurp for themselves the reins of government, destroying afterwards the very engines which have lifted them to unjust dominion.",
+    "author": "George Washington",
+    "source": "https://www.goodreads.com/quotes/462873-however-political-parties-may-now-and-then-answer-popular-ends"
+  },
+  {
+    "id": "insurgencies",
+    "quote": "&nbsp;&nbsp;&nbsp;One of these patterns in the history of many insurgent movements has been a disappointment in the direction that the movement has taken after victory, including claims that the revolution has been \"betrayed\" and that the later leaders and followers have failed to live up to the high standards set by their predecessors in the insurgency. None of this is surprising when such movements are examined in terms of their processes, including the incentives and constraints at work. <br><br>&nbsp;&nbsp;&nbsp;First of all, the kinds of people attracted to the original insurgency, under the initial set of incentives and constraints, tend to be very different from the kinds of people who gravitate to it after it has become successful and achieved a major part of its goals. By definition, an insurgent movement forms under a set of incentives and constraints very different from those which it seeks to create. Often the members face a certain amount of hostility, or even persecution, from those around them or from an elite currently benefitting from the status quo. These original insurgents may even face dangers to their careers or to their lives. These are not conditions which tend to attract timid careerists or mere opportunists, unless the opportunists foresee a high probability that the insurgency will succeed within period of time that is relevant to their personal ambitions. <br><br>&nbsp;&nbsp;&nbsp;After the success of the insurgency, however, radically different incentives and constraints are created by that very success. Many Christians in the Roman Empire, for example, went from being a poor and persecuted minority to being among the powerful agents of a state religion, able to enrich themselves and to persecute others. A similar pattern marked the history of the Communists in Russia many centuries later. On a smaller scale, the life cycle of regulatory agencies in the United States has often been seen to follow a pattern leading to control by very different kinds of people and policies from those behind the movement which led to the creation of the agency in the first place.",
+    "author": "Thomas Sowell, Knowledge and Decisions",
+    "source": "https://www.google.com/books/edition/Knowledge_And_Decisions/-a1UEAAAQBAJ?hl=en&gbpv=1&dq=revolution+leaders+goal+inauthor:Thomas+inauthor:Sowell&pg=PT10&printsec=frontcover",
+  },
+  {
+    "id": "champagne",
+    "quote": "If there must be pain in life, let it be champagne.",
+    "author": "Jude Law, The Talented Mr Ripley",
+    "source": "https://www.goodreads.com/quotes/462873-however-political-parties-may-now-and-then-answer-popular-ends"
+  },
+  {
+    "id": "sit-alone",
+    "quote": "All of humanity's problems stem from man's inability to sit quietly in a room alone.",
+    "author": "Blaise Pascal",
+    "source": "https://www.goodreads.com/quotes/19682-all-of-humanity-s-problems-stem-from-man-s-inability-to-sit"
+  },
+  {
+    "id": "one-is-a-statistic",
+    "quote": "One death is a tragedy; a million is a statistic.",
+    "author": "Joseph Stalin",
+    "source": "https://www.goodreads.com/quotes/140634-those-who-would-give-up-essential-liberty-to-purchase-a",
+    "related": ["https://www.goodreads.com/quotes/7253432-one-death-is-a-tragedy-a-million-is-a-statistic"]
+  },
+  {
+    "id": "liberty-safety",
+    "quote": "Those who would give up essential liberty to purchase a little temporary safety, deserve neither liberty nor safety.",
+    "author": "Benjamin Franklin",
+    "source": "https://www.goodreads.com/quotes/140634-those-who-would-give-up-essential-liberty-to-purchase-a"
+  },
+  {
+    "id": "price-of-freedom",
+    "quote": "The Price of Freedom Is Eternal Vigilance.",
+    "author": "John Philpot Curran; Thomas Jefferson",
+    "source": "https://www.goodreads.com/quotes/401-the-price-of-freedom-is-eternal-vigilance",
+    "related": ["https://www.monticello.org/research-education/thomas-jefferson-encyclopedia/eternal-vigilance-price-liberty-spurious-quotation/"]
+  },
+  {
+    "id": "studied-grace",
+    "quote": "Studied grace is not grace.",
+    "author": "Ottessa Moshfegh",
+    "source": "https://www.goodreads.com/book/show/44279110-my-year-of-rest-and-relaxation"
+  },
+  {
+    "id": "god-shaped-hole",
+    "quote": "There is a God-shaped vacuum in the heart of each man which cannot be satisfied by any created thing but only by God the Creator.",
+    "author": "Blaise Pascal",
+    "source": "https://www.goodreads.com/quotes/801132-there-is-a-god-shaped-vacuum-in-the-heart-of-each"
+  },
+  {
+    "id": "high-performance",
+    "quote": "Somehow, hours of poor performance are required for moments of high performance.",
+    "author": "Dominic Farolino",
+    "source": "https://domfarolino.com"
+  }
+];
