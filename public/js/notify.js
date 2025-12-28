bf02c446edbaa5d740b2b95e44f8ac429f19c9d6
Dominic Farolino <domfarolino@gmail.com>
2025-12-27 21:47:11 -0500

    Revert "Sticky sessions"
    This reverts commit 3a3cce47b0a02d11e6e8f8163f49385af864f43b.

--- public/js/notify.js
+++ public/js/notify.js
@@ -45,12 +45,9 @@ async function notifyClientOfVisit() {
     return;
   }
 
-  // Get or generate a sticky session ID that persists across page loads.
-  let sessionId = localStorage.getItem('currentVisitSessionId');
-  if (!sessionId) {
-    sessionId = generateSessionId();
-    localStorage.setItem('currentVisitSessionId', sessionId);
-  }
+  // Generate and store a new session ID for this visit.
+  const sessionId = generateSessionId();
+  localStorage.setItem('currentVisitSessionId', sessionId);
 
   const body = await getGeoData();
   body['referrer'] = document.referrer;
